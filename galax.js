5c21c399aed7635f67111c3113350970fc3e9476
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-10 22:54:56 +0300

    Update galax.js

--- galax.js
+++ galax.js
@@ -2,7 +2,7 @@
 //>* Adds one and multiples by 2
 //<< decrements the value by two
 //^ outputs the ASCII character of the value
-function galax(program,input){
+function galax(program){
     var output='';var acum=0;
   for(var p=0;p<program.length;p++){
     if (program[p]==">",program[p+1]=="*"){acum++;acum=acum*2;p++;}
